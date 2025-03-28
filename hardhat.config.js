2f7306c867028616975eb1721fa7e4d0525bd453
Agost Biro <agostbiro@gmail.com>
2025-03-28 20:10:12 +0000

    chore: update to hh v3 config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,10 +3,24 @@ module.exports = {
   solidity: {
     version: "0.8.24",
     settings: {
-      optimizer: false
+      optimizer: {enabled: false}
     }
   },
   paths: {
     sources: "./test",
+  },
+  solidityTest: {
+    testFail: true,
+    rpcEndpoints: {
+      mainnet: "https://eth.merkle.io",
+      optimism_sepolia: "https://sepolia.optimism.io/",
+      arbitrum_one_sepola: "https://sepolia-rollup.arbitrum.io/rpc/",
+      needs_undefined_env_var: "${UNDEFINED_RPC_URL_PLACEHOLDER}"
+    },
+    fuzz: {
+      // Used to ensure deterministic fuzz execution
+      seed: "0x1234567890123456789012345678901234567890",
+    },
   }
 };
+
